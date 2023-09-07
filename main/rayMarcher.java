01914e03dedad94750dc9956d7497f6847ae86f0
BuildTools <unconfigured@null.spigotmc.org>
2023-09-06 20:45:57 -0400

    e

--- main/rayMarcher.java
+++ main/rayMarcher.java
@@ -1,22 +1,36 @@
 package main;
 
+
+
 public class rayMarcher {
-   public static void main(String[]args)
-   {     
-        
-   }
 
    public static int[] getPixel(double[] worldData, double[] screenPixel)
    {
-      double rx = worldData[0];
+      double rx = worldData[0]; //import world data and set up vars
       double ry = worldData[1];
       double rz = worldData[2];
       int[] colorStorge = {0,0,0};
       double xyRotation = worldData[3];
       double zRotation = worldData[4];
-      double fov = worldData[5];
+      double fov = (worldData[5]/2);
+      boolean isWorking = true;
+
+
+      double tempRotation = xyRotation + (screenPixel[0] * fov); //set up xyz velocitys/rotation
+      double rzv = Math.sin(zRotation + (screenPixel[1] * fov));
+      double rxv = Math.cos(tempRotation);
+      double ryv = Math.sin(tempRotation);
       
-      
+      while(isWorking);
+         {
+   
+
+
+
+
+         }
+
+
 
       return colorStorge;
    }
