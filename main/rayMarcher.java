1e255f076842d0014bd57f028ef9bc370a829862
BuildTools <unconfigured@null.spigotmc.org>
2023-09-06 21:09:37 -0400

    b

--- main/rayMarcher.java
+++ main/rayMarcher.java
@@ -13,13 +13,13 @@ public class rayMarcher {
       double xyRotation = worldData[3];
       double zRotation = worldData[4];
       double fov = (worldData[5]/2);
-      boolean isWorking = true;
+      boolean isWorking = false;
 
 
       double tempRotation = xyRotation + (screenPixel[0] * fov); //set up xyz velocitys/rotation
-      double rzv = Math.sin(zRotation + (screenPixel[1] * fov));
-      double rxv = Math.cos(tempRotation);
-      double ryv = Math.sin(tempRotation);
+      double rzv = Math.sin(Math.toRadians(zRotation + (screenPixel[1] * fov)));
+      double rxv = Math.cos(Math.toRadians(tempRotation));
+      double ryv = Math.sin(Math.toRadians(tempRotation));
       
       while(isWorking);
          {
--- main/test.java
+++ main/test.java
@@ -1,8 +1,20 @@
 package main;
-
+import main.rayMarcher;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for (double ix = 0; ix < 16; ix++) {
+            for (double iy = 0; iy < 16; iy++) {
+                getPixel(null,null);
+
+
+
+
+
+            }
+
+        }
+    
+
     }
 }
