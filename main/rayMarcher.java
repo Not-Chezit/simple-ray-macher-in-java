970176708e6fc386be7313ad2dd75e6f8fc0591d
BuildTools <unconfigured@null.spigotmc.org>
2023-09-07 19:48:32 -0400

    biggie

--- main/rayMarcher.java
+++ main/rayMarcher.java
@@ -12,18 +12,18 @@ public class rayMarcher {
       double[] colorStorge = null;
       double xyRotation = worldData[3];
       double zRotation = worldData[4];
-      double fov = (worldData[5]/2);
+      double fov = (worldData[5]/1);
      
       boolean isWorking = true;
       boolean isSearching = true;
       double rayAge = 0;
       double stepSize = 1;
-
+      double cheapAO = 0;
 
       double tempRotation = xyRotation + (screenPixel[0] * fov); //set up xyz velocitys/rotation
       double rzv = Math.sin(Math.toRadians(zRotation + (screenPixel[1] * fov)));
-      double rxv = Math.cos(Math.toRadians(tempRotation));
-      double ryv = Math.sin(Math.toRadians(tempRotation));
+      double rxv = Math.cos(Math.toRadians(tempRotation)) * Math.cos(Math.toRadians(zRotation + (screenPixel[1] * fov)));
+      double ryv = Math.sin(Math.toRadians(tempRotation)) * Math.cos(Math.toRadians(zRotation + (screenPixel[1] * fov)));
       
       //simpleShape tempShape = new simpleShape();//temp stuff
       double tempWorldData[] = worldData.clone();
@@ -34,7 +34,7 @@ public class rayMarcher {
       while(isWorking)
          {
             
-            
+            cheapAO ++;
             stepSize = 1;
             tempWorldData[0] = rx;
             tempWorldData[1] = ry;
@@ -61,7 +61,17 @@ public class rayMarcher {
             tempWorldData[2] = rz;
             colorStorge =  simpleShape.cube(tempWorldData, tempPos, tempSize, tempTex);
             
-            if(colorStorge[0] == 6.9){isWorking = false;}
+            if(colorStorge[0] == 6.9){
+               isWorking = false;
+               cheapAO *= 5;
+               colorStorge[1] -= cheapAO;
+               colorStorge[2] -= cheapAO;
+               colorStorge[3] -= cheapAO;
+               if(colorStorge[1] < 0){colorStorge[1] = 0;}
+               if(colorStorge[2] < 0){colorStorge[2] = 0;}
+               if(colorStorge[3] < 0){colorStorge[3] = 0;}
+
+            }
             if(rayAge >= 100){isWorking = false;}
          }
 
--- main/test.java
+++ main/test.java
@@ -8,7 +8,7 @@ public class test {
     public static void main(String[] args) throws IOException {
         //rayMarcher worker1 = new rayMarcher();
         
-        String filename = "main\\imgsrc\\shane.png";
+        String filename = "main\\imgsrc\\360p.png";
         Imager image1 = new Imager(filename); //Object to be created per image file
         Color color = image1.getPixelColor(0,0); //Example getPixelColor call
        // image1.setPixelColor(0,0, new Color(255, 0, 0)); //Example setPixelColor call
@@ -16,14 +16,15 @@ public class test {
 
 
 
-        double[] temp = {0,0,0,-15,0,90};
+        double[] temp = {0,30,-10,-30,0,90};
         double[] screenpos = {0,0};
         double[] tempOutput = {0,0,0,0};
 
         for (double i = 0; i < 640; i++) {
             for (double j = 0; j < 360; j++) {
-                screenpos[0] = (i/500)-0.5;
-                screenpos[1] = (j/500)-0.5;
+                screenpos[0] = (i/320)-0.5;
+                screenpos[1] = (j/320)-0.5;
+                screenpos[1] *= -1;
                 tempOutput = rayMarcher.getPixel(temp, screenpos);
                // System.out.print(tempOutput[0] + " ");
                 image1.setPixelColor((int)i, (int)j, new Color((int)tempOutput[1], (int)tempOutput[2], (int)tempOutput[3]));
