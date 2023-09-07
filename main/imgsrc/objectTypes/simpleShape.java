662b03f536079df897f7973f7d8bf16db3cc4440
BuildTools <unconfigured@null.spigotmc.org>
2023-09-06 21:18:42 -0400

    e

--- main/imgsrc/objectTypes/simpleShape.java
+++ main/imgsrc/objectTypes/simpleShape.java
@@ -1,7 +1,7 @@
 package main.imgsrc.objectTypes;
 
 public class simpleShape{
-    public static double[] cube(double[] worldData, double[] pos, double size){
+    public static double[] cube(double[] worldData, double[] pos, double size, String texture){
         double[] output = {0,0,0,0};
         
 
--- main/test.java
+++ main/test.java
@@ -3,15 +3,21 @@ import main.rayMarcher;
 
 public class test {
     public static void main(String[] args) {
+        rayMarcher worker1 = new rayMarcher();
+        
         for (double ix = 0; ix < 16; ix++) {
             for (double iy = 0; iy < 16; iy++) {
-                getPixel(null,null);
+                
+
+
+                System.out.print(worker1.getPixel(null, null));
 
 
 
 
 
             }
+            System.out.println();
 
         }
     
