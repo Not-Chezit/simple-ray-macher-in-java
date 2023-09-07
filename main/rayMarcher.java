710d5a8d920db123c04dde3590c130fecc53a07f
BuildTools <unconfigured@null.spigotmc.org>
2023-09-06 20:06:13 -0400

    BS

---
+++ main/rayMarcher.java
@@ -0,0 +1,22 @@
+package main;
+
+public class rayMarcher {
+   public static void main(String[]args)
+   {     
+        
+   }
+
+   public static int[] getPixel(double[] worldData, double[] screenPixel)
+   {
+      double rx = worldData[0];
+      double ry = worldData[1];
+      double rz = worldData[2];
+      double xyRotation = worldData[3];
+      double zRotation = worldData[4];
+      double fov = worldData[5];
+      
+      
+
+
+   }
+}  
