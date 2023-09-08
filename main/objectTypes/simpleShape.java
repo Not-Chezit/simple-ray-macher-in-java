3f58013ffb4a1c3c801e9f57b5ed01763eb1d5df
BuildTools <unconfigured@null.spigotmc.org>
2023-09-07 20:12:10 -0400

    b

--- main/objectTypes/simpleShape.java
+++ main/objectTypes/simpleShape.java
@@ -1,12 +1,17 @@
 package main.objectTypes;
 
+import main.Imager;
+import java.awt.Color;
+
 public class simpleShape{
+    
     public static double[] cube(double[] worldData, double[] pos, double size, String texture){
         double[] output = {0,0,0,0};
         double rx = worldData[0];
         double ry = worldData[1];
         double rz = worldData[2];
         if (((pos[0] - size) <= rx) && (rx <= (pos[0] + size))&&((pos[1] - size) <= ry )&&( ry <= (pos[1] + size))&&((pos[2] - size) <= rz )&&( rz <= (pos[2] + size))) {
+
 
             output[0] = 6.9;
             output[1] = 200;
